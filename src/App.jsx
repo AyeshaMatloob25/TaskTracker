601a6c4a5511e18aa9fbbcf83f52507325cea22e
Ayesha Matloob <ayesha.matloob96@gmail.com>
2021-12-08 15:05:55 +0100

    Add header  and button components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import Header from "./components/Header";
+
+
+const initialTasks = [
+  {
+    id: 1,
+    text: "Task 1",
+    day: "Dec 10th at 2:00 pm",
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: "Task 2",
+    day: "Dec 12th at 6:00 pm",
+    reminder: false,
+  },
+  {
+    id: 3,
+    text: "Task 3",
+    day: "Dec 18th at 3:00 pm",
+    reminder: true,
+  },
+];
+
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="container">
+     <Header/>
     </div>
   );
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+
+function Button({children,onClick, color}) {
+    return (
+        <button style={{backgroundColor: color}} 
+        onClick ={onClick}
+        className="btn">
+            {children}
+        </button>
+    )
+}
+Button.defaultProps={
+    color: "steelBlue",
+};
+Button.propTypes = {
+    children: PropTypes.node,
+    onClick: PropTypes.func,
+    color: PropTypes.string,
+};
+export default Button;
