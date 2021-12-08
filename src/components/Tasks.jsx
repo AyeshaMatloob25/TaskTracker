6cbccb4c4921ac830a86f3aadd3de6ad1ff9e2ae
Ayesha Matloob <ayesha.matloob96@gmail.com>
2021-12-08 15:23:31 +0100

    Add tasks component

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,14 @@
+import PropTypes from "prop-types";
+
+function Tasks({tasks}){
+    return tasks.map((task) => 
+        <p key={task.id}>{task.text}</p>);
+} 
+Tasks.defaultProps ={
+    tasks:[],
+};
+
+Tasks.propTypes={
+    tasks:PropTypes.array,
+};
+export default Tasks;
