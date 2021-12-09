cf988439984f4c751ec3543eab8d10b003c9ac1d
Ayesha Matloob <ayesha.matloob96@gmail.com>
2021-12-09 15:15:22 +0100

    Add toggle reminder functionality

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,10 +1,10 @@
 import PropTypes from "prop-types";
 import { FaTimes } from "react-icons/fa";
 
-function Task({ task }){
+function Task({ task, onToggle }){
     const containerClasses = "task " + (task.reminder ? "reminder" : "");
     return (
-        <div className={containerClasses}>
+        <div onDoubleClick={()=> onToggle(task.id)}  className={containerClasses}>
             <h3>
                 {task.text} 
                 <FaTimes style={ {color : "red", cursor : "pointer" }}/>
@@ -21,5 +21,6 @@ Task.propTypes = {
         day: PropTypes.string.isRequired,
         reminder: PropTypes.bool.isRequired,
     }).isRequired,
+    onToggle: PropTypes.func.isRequired,
 };
  export default Task;
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,12 +1,16 @@
 import PropTypes from "prop-types";
 import Task from "./Task";
 
-function Tasks({tasks}){
+function Tasks({tasks, onToggle}){
     if(!tasks || tasks.length === 0) {
         return <p> No Tasks to display </p>;
     }
     return tasks.map((task) => 
-    <Task key= {task.id} task={task} />);
+    <Task 
+    onToggle={onToggle} 
+    key= {task.id} 
+    task={task} 
+    />);
 } 
 Tasks.defaultProps ={
     tasks:[],
@@ -14,5 +18,6 @@ Tasks.defaultProps ={
 
 Tasks.propTypes={
     tasks:PropTypes.array,
+    onToggle: PropTypes.func.isRequired,
 };
 export default Tasks;
